e716033c0b23553eaa78d014d8476d1c5953b74e
Jessica <jessicanevesmachado@gmail.com>
2022-03-10 07:35:05 -0300

    fix: 🐛 eslint

--- src/data/usecases/authentication/remote-authentication.test.ts
+++ src/data/usecases/authentication/remote-authentication.test.ts
@@ -2,6 +2,8 @@ import { HttpPostClientSpy } from '@data/test/mock-http-client'
 import { RemoteAuthentication } from '@data/usecases/authentication/remote-authentication'
 import faker from 'faker'
 import { mockAuthentication } from '@domain/test/mock-authentication'
+import { InvalidCredentialsError } from '@domain/errors/InvalidCredentialsError'
+import { HttpStatusCode } from '@data/protocols/http/http-response'
 
 type SutTypes = {
   remoteAuthentication: RemoteAuthentication
@@ -9,25 +11,36 @@ type SutTypes = {
 }
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy()
-  const remoteAuthentication = new RemoteAuthentication(url,httpPostClientSpy)
+  const remoteAuthentication = new RemoteAuthentication(url, httpPostClientSpy)
   return {
     remoteAuthentication,
     httpPostClientSpy
   }
 }
 
-describe('RemoteAuthentication',() => {
-  it('Shoul call HttpClient with correct URL',() => {
+describe('RemoteAuthentication', () => {
+  it('Shoul call HttpClient with correct URL', async () => {
     const url = faker.internet.url()
-    const { remoteAuthentication,httpPostClientSpy } = makeSut(url)
+    const { remoteAuthentication, httpPostClientSpy } = makeSut(url)
     remoteAuthentication.auth(mockAuthentication())
-    expect(httpPostClientSpy.url).toBe(url)
+    await expect(httpPostClientSpy.url).toBe(url)
   })
 
-  it('Should call HttpClient with correct body',() => {
+  it('Should call HttpClient with correct body', async () => {
     const authenticationParams = mockAuthentication()
-    const { remoteAuthentication,httpPostClientSpy } = makeSut()
+    const { remoteAuthentication, httpPostClientSpy } = makeSut()
     remoteAuthentication.auth(authenticationParams)
-    expect(httpPostClientSpy.body).toEqual(authenticationParams)
+    await expect(httpPostClientSpy.body).toEqual(authenticationParams)
+  })
+
+  it('Should throw invalidCredentialsError if HttpPostClient returns 401', async () => {
+    const { remoteAuthentication, httpPostClientSpy } = makeSut()
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.unauthorized
+    }
+
+    const promisse = remoteAuthentication.auth(mockAuthentication())
+    await expect(promisse).rejects.toThrow(new InvalidCredentialsError())
   })
 })
