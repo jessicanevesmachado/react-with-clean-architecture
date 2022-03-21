88e1de0f199165a689f99bd6fd097cc8964edf53
Jessica <jessicanevesmachado@gmail.com>
2022-03-21 09:19:11 -0300

    refactor: 💡 add type to any

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -3,7 +3,7 @@ import { HttpResponse } from ".";
 // interface segregation principle : interfaces pequenas. so com o post sem o get
 export type HttpPostParams<T> = {
   url: string;
-  body?: object;
+  body?: any;
 };
 export interface HttpPostClient<T, R> {
   post: (params: HttpPostParams<T>) => Promise<HttpResponse<R>>;
